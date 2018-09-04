d9545c4462a4ad7c61255a04d5c4cffc21d79dc3
david roland <david.roland@atticuscreative.co.uk>
2018-09-04 15:12:50 +0100

    my first commit

---
+++ inc/customizer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PsychiatryGroup Theme Customizer
+ *
+ * @package PsychiatryGroup
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function psychiatrygroup_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'psychiatrygroup_customize_register' );
